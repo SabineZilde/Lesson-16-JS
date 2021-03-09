9992ce4f6dce1c6c23754f0b82788a1035257cf8
SabineZilde <sabine.zilde@protonmail.ch>
2021-03-09 21:20:16 +0200

    Initial commit

---
+++ Task1-Print/js/js/main.js
@@ -0,0 +1,5 @@
+const myName = 'Sabīne';
+const myAddress = 'Fiji 123';
+const favNum = 8;
+
+console.log('Hello, my name is ' + myName + ', I live in ' + myAddress + ' and my favorite number is ' + favNum + '!');
---
+++ Task2-Array/js/main.js
@@ -0,0 +1,6 @@
+const myCars = ['Vaz', 'Zaz', 'Gaz', 'Uaz', 'Maz'];
+console.log('5th car is: ' + myCars[4]);
+console.log('4th car is: ' + myCars[3]);
+console.log('3rd car is: ' + myCars[2]);
+console.log('2nd car is: ' + myCars[1]);
+console.log('1st car is: ' + myCars[0]);
